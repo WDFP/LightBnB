31266abe4065c7d340f87d8f446af6f2a10bf904
Victor Ha <victorha88@gmail.com>
2023-01-02 15:55:05 -0500

    Tree traversal-Breadth vs. Depth

--- durian-tree.js
+++ durian-tree.js
@@ -28,6 +28,21 @@ class Employee {
   hasSameBoss(employee) {
     return this.boss === employee.boss;
   }
+  employeesThatMakeOver(amount) {
+
+    let employees = []; // 1
+
+    if (this.salary > amount) {
+      employees.push(this); // 2
+    }
+
+    for (const subordinate of this.subordinates) {
+      const subordinatesThatMakeOver = subordinate.employeesThatMakeOver(amount); // 3
+      employees = employees.concat(subordinatesThatMakeOver);
+    }
+
+    return employees;
+  }
 }
 const ada = new Employee("Ada", "CEO", 3000000.00);
 
