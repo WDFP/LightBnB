ba827a371ff5c08943f22b5ed7ab106e3b68b6d8
Victor Ha <victorha88@gmail.com>
2023-01-06 14:37:18 -0500

    Changed addProperty function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -37,6 +37,7 @@ const getUserWithId = function(id) {
     return result.rows[0];
   })
   .catch((err) => {
+    console.log(err);
     return null;
   });
 }
@@ -49,7 +50,8 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  return pool.query(`INSERT INTO users (name, email, password) VALUES($1, $2, $3)
+  return pool.query(`INSERT INTO users (name, email, password)
+  VALUES($1, $2, $3)
   RETURNING *`, 
   [user.name, user.email, user.password])
   .then((result) => {
@@ -143,9 +145,28 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  return pool.query (`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *`,
+    [
+     property.owner_id,
+     property.title,
+     property.description,
+     property.thumbnail_photo_url,
+     property.cover_photo_url,
+     property.cost_per_night,
+     property.parking_spaces,
+     property.number_of_bathrooms,
+     property.number_of_bedrooms,
+     property.country,
+     property.street,
+     property.city,
+     property.province,
+     property.post_code,
+   ])
+   .then((result) => {  
+    return result.rows[0];
+   });
+  };
 exports.addProperty = addProperty;
